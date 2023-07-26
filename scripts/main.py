3e9713d3e04aef386dec5a80a751aae650999d09
MaruKazeMaru <kaze47111@gmail.com>
2023-07-26 14:09:29 +0900

    update

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,43 @@
-from .get_pcs import pca_result
+import argparse
+
+import principal_component_analysis as pca
+
+def main(args):
+    datas = []
+    with open(args.datafile, mode="r") as f:
+        for line in f:
+            data = [float(s) for s in line.split(' ')]
+            datas.append(data)
+
+    a = pca.pca_assister(datas)
+    result = a.pca_dim(args.dim)
+
+    if args.show:
+        print("avr=")
+        print(a.avr)
+        print("eig_vals=")
+        print(a.eig_vals)
+        print("eig_vecs=")
+        print(a.eig_vecs)
+
+    for i in range(args.dim):
+        p:str = args.datafile
+        ps = p.split("/")
+        fname = ps[-1]
+        l = fname.split(".")
+        l[0] += "_pca" + str(i+1)
+        fname = ".".join(l)
+        ps[-1] = fname
+        p = "/".join(ps)
+        with open(p, "w") as f:
+            for j in range(len(result)):
+                f.write(str(result[j][i]) + "\n")
+
 
 if __name__ == "__main__":
-    data_size = int(input())
-    if data_size <= 0:
-        raise ValueError("data_size <= 0")
-    
-    datas:list[list[float]] = []
-    for i in range(data_size):
-        data_l = input().split(' ')
-        datas.append([float(s) for s in data_l])
-
-    res = pca_result(datas)
-    for
+    parser =  argparse.ArgumentParser(description="pricial component analysis")
+    parser.add_argument("datafile", help="path of data file")
+    parser.add_argument("-d", "--dim", help="pca max dimension", type=int, default=1)
+    parser.add_argument("-s", "--show", help="show average & eigen values & eigen vectors", action="store_true")
+    args = parser.parse_args()
+    main(args)
---
+++ scripts/principal_component_analysis.py
@@ -0,0 +1,61 @@
+import numpy as np
+import numpy.linalg as LA
+
+class pca_assister:
+    dim:int
+    avr:np.ndarray
+    eig_vals:np.ndarray
+    eig_vecs:np.ndarray
+    datas_mat:np.ndarray
+
+    def __init__(self, datas:list[list[float]]):
+        x = np.array(datas)
+
+        self.dim = x.shape[1]
+        datas_size = x.shape[0]
+
+        self.avr = np.ndarray((self.dim,1))
+
+        for i in range(self.dim):
+            s = sum(x[:,i])
+            self.avr[i,0] = s / float(datas_size)
+            
+        x = x.T
+        x -= self.avr
+
+        m = x @ x.T
+        (self.eig_vals, self.eig_vecs) = LA.eig(m)
+
+        s = np.argsort(-self.eig_vals)
+
+        self.eig_vecs = self.eig_vecs.T
+        self.eig_vecs = self.eig_vecs[s]
+        self.eig_vecs = self.eig_vecs.T
+
+        self.datas_mat = x
+
+
+    def _pca_simple(self, d:int) -> list[list[float]]:
+        m = self.eig_vecs[:,0:d]
+        v = self.avr.T @ m
+        r = self.datas_mat.T @ m
+        r -= v
+
+        return r.tolist()
+
+
+    def pca_dim(self, how_many_dim:int) -> list[list[float]]:
+        if how_many_dim <= 0:
+            return []
+    
+        d = min(self.dim, how_many_dim)
+
+        return self._pca_simple(d)
+
+"""
+def pca_pareto(datas:list[list[float]], ratio:float):
+    if ratio <= 0.0:
+        return []
+    
+
+"""
